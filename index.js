f91dde003f11b0ac38f2a3a20f7f80275dc162f7
Matthew Listor <mattlistor@gmail.com>
2019-04-20 02:32:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  object[key] = value
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+  newObj = Object.assign({}, obj)
+  newObj[key] = value
+  return newObj
 }
 
 function deleteFromObjectByKey(object, key) {
